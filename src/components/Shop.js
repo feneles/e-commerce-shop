e9cb471c740e473cdae4ffe5b0ba597c5a37d6d7
Marek Rogala <rogala.marek91@gmail.com>
2020-09-01 18:26:25 +0200

    .

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -8,12 +8,24 @@ function Shop({ match }) {
   useEffect(() => {
     const filterItems = Data.find((section) => section.section === sectionName);
     setItems(filterItems);
-    console.log(items);
+    console.log(items.products);
   }, [match, sectionName, items]);
+
+  const clothes =
+    items.length > 0
+      ? items.products.map((item) => (
+          <div>
+            <img src={item.img} alt="" />
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </div>
+        ))
+      : null;
 
   return (
     <div className="shop">
       <h1>{match.params.id}</h1>
+      {clothes}
     </div>
   );
 }
