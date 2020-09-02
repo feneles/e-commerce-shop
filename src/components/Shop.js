396624a92d86e0b2d31c6724b34fa07796f31455
Lukortech <lukortech@wp.pl>
2020-09-02 21:57:54 +0200

    min(small cleanup in code and fix in shop.js)

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,31 +2,29 @@ import React, { useState, useEffect } from "react";
 import data from "../data.json";
 
 function Shop({ match }) {
-  const [items, setItems] = useState({});
-  const [sectionName] = useState(match.params.id);
+  const [products, setProducts] = useState([]);
+  const [sectionName, setSectionName] = useState(match.params.id);
 
   useEffect(() => {
-    const filterItems = data.find((section) => section.section === sectionName);
-    setItems(filterItems);
-    console.log(items);
-  }, [match.url]);
+    const sectionSpecificFilteredData = data.find((section) => section.section === sectionName);
+    const { products } = sectionSpecificFilteredData;
+    
+    setProducts(products);
+  });
 
-  console.log(match);
-
-  const clothes =
-    items.length > 0 &&
-    items.products.map((item) => (
-      <div style={{ height: "200px", width: "200px" }}>
-        <img src={item.img} alt={item.description} />
-        <p>{item.description}</p>
-        <p>{item.price}</p>
-      </div>
-    ));
+  const ProductDetails = ({ product }) => {
+    debugger;
+    return (<div className="product-details" style={{ height: "200px", width: "200px" }}>
+      <img src={product.img} alt={product.description} />
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+    </div>)
+  };
 
   return (
     <div className="shop">
       <h1>{match.params.id}</h1>
-      {clothes}
+      {products.length > 0 && products.map((product, index) => <ProductDetails key={index} product={product} />)}
     </div>
   );
 }
