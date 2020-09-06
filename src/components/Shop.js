0d0c5f74b8df1e3632503dd54e2ca9b75041110d
Marek Rogala <rogala.marek91@gmail.com>
2020-09-06 11:24:41 +0200

    fix loading screen

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react";
 import data from "../data.json";
 import Button from "@material-ui/core/Button";
+import loadingIcon from "@iconify/icons-codicon/loading";
+import { Icon } from "@iconify/react";
 
 function Shop(props) {
   const [products, setProducts] = useState([]);
   const [sectionName, setSectionName] = useState(props.match.params.id);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const sectionSpecificFilteredData = data.find(
       (section) => section.section === sectionName
     );
     const { products } = sectionSpecificFilteredData;
     setProducts(products);
+    setIsLoading(false);
   }, [sectionName]);
 
   useEffect(() => {
@@ -64,10 +69,19 @@ function Shop(props) {
 
   return (
     <div className="shop">
-      {products.length > 0 &&
+      {isLoading ? (
+        <div className="loading">
+          <Icon
+            icon={loadingIcon}
+            style={{ color: "#666", fontSize: "70px" }}
+          />
+        </div>
+      ) : (
+        products.length > 0 &&
         products.map((product, index) => (
           <ProductDetails key={index} product={product} />
-        ))}
+        ))
+      )}
     </div>
   );
 }
